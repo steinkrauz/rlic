cda2a491c1c0ad97f01fc19bb9bef0d3fdc83953
steinkrauz <steinkrauz@localhost>
2011-12-29 09:40:20 +0400

    Skeleton for config editor

---
+++ src/com/scac/RLicServer/RLicCfgGUI.java
@@ -0,0 +1,28 @@
+package com.scac.RLicServer;
+
+import javax.swing.JFrame;
+
+public class RLicCfgGUI extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new RLicCfgGUI();
+
+	}
+	
+	public RLicCfgGUI() {
+		// TODO Auto-generated constructor stub
+		super("Fuck you");
+		setSize(300, 600);
+		setVisible(true);
+	}
+
+}
